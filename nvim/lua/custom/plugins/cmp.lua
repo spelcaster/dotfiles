56a54123b16934f5e34b3d79c70b4354a387cde2
Hugo do Carmo <hugrito.kun@gmail.com>
2026-06-23 10:12:25 -0300

    feat(nvim): add nvim-cmp completion setup
    configure nvim-cmp with lsp, buffer, path and luasnip sources
    load friendly-snippets and map navigation keys

---
+++ nvim/lua/custom/plugins/cmp.lua
@@ -0,0 +1,57 @@
+vim.pack.add({
+  { src = 'https://github.com/hrsh7th/nvim-cmp' },
+  { src = 'https://github.com/hrsh7th/cmp-nvim-lsp' },
+  { src = 'https://github.com/hrsh7th/cmp-buffer' },
+  { src = 'https://github.com/hrsh7th/cmp-path' },
+
+  { src = 'https://github.com/rafamadriz/friendly-snippets' },
+  { src = 'https://github.com/saadparwaiz1/cmp_luasnip' },
+})
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    ['<CR>'] = cmp.mapping.confirm({
+      select = true,
+    }),
+
+    ['<C-j>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ['<C-k>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'path' },
+    { name = 'buffer' },
+  }),
+})
