1fc526f6bbbad7336e632db9ec608a22acbae4f7
Hugo do Carmo <hugrito.kun@gmail.com>
2026-06-23 10:12:25 -0300

    feat(nvim): add nvim-dap debugging plugins
    add nvim-dap, dap-ui, dap-virtual-text and nvim-nio

---
+++ nvim/lua/custom/plugins/debugger.lua
@@ -0,0 +1,6 @@
+vim.pack.add({
+  { src = "https://github.com/mfussenegger/nvim-dap" },
+  { src = "https://github.com/rcarriga/nvim-dap-ui" },
+  { src = "https://github.com/theHamsta/nvim-dap-virtual-text" },
+  { src = "https://github.com/nvim-neotest/nvim-nio" },
+})
