f01e71b78fc5feeeb589608f67b6138a1e8ba194
Hugo do Carmo <hugrito.kun@gmail.com>
2026-08-12 11:33:16 -0300

    feat(nvim): load custom plugins in explicit order via init.lua
    override kickstart's default plugin loader (which iterates every lua
    file in custom/plugins/) with an explicit list of require calls, since
    plugin load order matters for this setup (e.g. lsp before debugger)
    and picking that order implicitly by file iteration is fragile

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,23 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+-- Iterate over all Lua files in the plugins directory and load them
+-- local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
+-- for file_name, type in vim.fs.dir(plugins_dir) do
+--   if type == 'file' and file_name:match '%.lua$' and file_name ~= 'init.lua' then
+--     local module = file_name:gsub('%.lua$', '')
+--     require('custom.plugins.' .. module)
+--   end
+-- end
+
+-- Override kickstart.nvim approach because:
+-- 1. I know which scripts I want, and 
+-- 2. the order which script is loaded matters
+require("custom.plugins.indent")
+require("custom.plugins.cmp")
+require("custom.plugins.lsp")
+-- require("custom.plugins.copilot")
+require("custom.plugins.debugger")
+require("custom.plugins.plugins")
